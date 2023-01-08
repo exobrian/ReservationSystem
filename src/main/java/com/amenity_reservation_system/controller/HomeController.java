64b689fa7615ccaf8b1a92c1514da1367d4d8a84
Brian Tran <exobrian@yahoo.com>
2023-01-07 18:57:53 -0800

    Revert "something broken"
    This reverts commit 3948943df209e6104ef5350b731bccf32f5c381e.

--- src/main/java/com/amenity_reservation_system/controller/HomeController.java
+++ src/main/java/com/amenity_reservation_system/controller/HomeController.java
@@ -3,15 +3,17 @@ package com.amenity_reservation_system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String index(Model model) {
 
-        return "index";
+        return "index.html";
     }
 }
