3948943df209e6104ef5350b731bccf32f5c381e
Brian Tran <exobrian@yahoo.com>
2023-01-07 18:57:38 -0800

    something broken

--- src/main/java/com/amenity_reservation_system/controller/HomeController.java
+++ src/main/java/com/amenity_reservation_system/controller/HomeController.java
@@ -3,17 +3,15 @@ package com.amenity_reservation_system.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @GetMapping("/")
     public String index(Model model) {
 
-        return "index.html";
+        return "index";
     }
 }
